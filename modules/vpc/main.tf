b5a4ae76f9bb1cd9a217c06e4dc4e2318a2302cb
koteswaraRao559 <koteswararao1999@gmail.com>
2024-06-27 21:31:55 +0530

    4th commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -65,26 +65,26 @@ resource "aws_route_table_association" "public"
 {
     count = length(aws_subnet.public)
     subnet_id      = aws_subnet.public.*.id[count.index]
-    route_table_id = aws_route_table.public.vpc_id
+    route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "web"
 {
   count = length(aws_subnet.web)
   subnet_id      = aws_subnet.web.*.id[count.index]
-  route_table_id = aws_route_table.web.vpc_id
+  route_table_id = aws_route_table.web.id
 }
 
 resource "aws_route_table_association" "db"
 {
   count = length(aws_subnet.db)
   subnet_id      = aws_subnet.db.*.id[count.index]
-  route_table_id = aws_route_table.db.vpc_id
+  route_table_id = aws_route_table.db.id
 }
 
 resource "aws_route_table_association" "app"
 {
   count = length(aws_subnet.app)
   subnet_id      = aws_subnet.app.*.id[count.index]
-  route_table_id = aws_route_table.app.vpc_id
+  route_table_id = aws_route_table.app.id
 }
