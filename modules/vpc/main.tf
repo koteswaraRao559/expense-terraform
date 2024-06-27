a602e0ed1b997037e1a16dc77617275658cfb9eb
koteswaraRao559 <koteswararao1999@gmail.com>
2024-06-27 21:26:26 +0530

    4th commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "main" {
 }
 
 # This subnet block need to nbe improvement in code dry concept
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public" {
   count             = length(var.public_subnets)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnets[count.index]
@@ -20,6 +20,16 @@ resource "aws_subnet" "web" {
   availability_zone = var.azs[count.index]
 }
 
+
+
+resource "aws_subnet" "db" {
+  count             = length(var.db_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.db_subnets[count.index]
+  tags              = merge(var.tags, { Name = "db_subnet"})
+  availability_zone = var.azs[count.index]
+}
+
 resource "aws_subnet" "app" {
   count             = length(var.app_subnets)
   vpc_id            = aws_vpc.main.id
@@ -28,10 +38,53 @@ resource "aws_subnet" "app" {
   availability_zone = var.azs[count.index]
 }
 
-resource "aws_subnet" "db" {
-  count             = length(var.db_subnets)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.db_subnets[count.index]
-  tags              = merge(var.tags, { Name = "db_subnet"})
-  availability_zone = var.azs[count.index]
+# step 3. creating 4 route tables
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, { Name = "public"})
+}
+
+resource "aws_route_table" "web" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, { Name = "web"})
+}
+
+resource "aws_route_table" "db" {
+  vpc_id = aws_vpc.main.id
+  tags   = merge(var.tags, { Name = "db" })
+}
+
+resource "aws_route_table" "app" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, { Name = "app"})
+}
+
+
+#step 4. Associating route tables with subnets
+resource "aws_route_table_association" "public"
+{
+    count = length(aws_subnet.public)
+    subnet_id      = aws_subnet.public.*.id[count.index]
+    route_table_id = aws_route_table.public.vpc_id
+}
+
+resource "aws_route_table_association" "web"
+{
+  count = length(aws_subnet.web)
+  subnet_id      = aws_subnet.web.*.id[count.index]
+  route_table_id = aws_route_table.web.vpc_id
+}
+
+resource "aws_route_table_association" "db"
+{
+  count = length(aws_subnet.db)
+  subnet_id      = aws_subnet.db.*.id[count.index]
+  route_table_id = aws_route_table.db.vpc_id
+}
+
+resource "aws_route_table_association" "app"
+{
+  count = length(aws_subnet.app)
+  subnet_id      = aws_subnet.app.*.id[count.index]
+  route_table_id = aws_route_table.app.vpc_id
 }
