cb770e64c2a6e57be987d8405acc3226e462dd44
koteswaraRao559 <koteswararao1999@gmail.com>
2024-06-27 20:48:56 +0530

    4th commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,5 +9,28 @@ resource "aws_subnet" "main" {
   cidr_block        = var.public_subnets[count.index]
   tags              = merge(var.tags, { Name = "public_subnet"})
   availability_zone = var.azs[count.index]
+}
 
+resource "aws_subnet" "web" {
+  count             = length(var.web_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.web_subnets[count.index]
+  tags              = merge(var.tags, { Name = "web_subnet"})
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "app" {
+  count             = length(var.app_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.app_subnets[count.index]
+  tags              = merge(var.tags, { Name = "app_subnet"})
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "db" {
+  count             = length(var.db_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.db_subnets[count.index]
+  tags              = merge(var.tags, { Name = "db_subnet"})
+  availability_zone = var.azs[count.index]
 }
