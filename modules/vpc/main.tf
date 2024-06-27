99ce8a692d3388aaa983eaa056459312ca38ca95
koteswaraRao559 <koteswararao1999@gmail.com>
2024-06-27 18:01:18 +0530

    4th commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,4 +7,5 @@ resource "aws_subnet" "main" {
   count = length(var.public_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index]
+  tags = merge(var.tags, { Name = "public_subnet"})
 }
