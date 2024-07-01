530e6061a8d53ed24177666b5d96a1eb2002a7ea
koteswaraRao559 <koteswararao1999@gmail.com>
2024-07-02 00:18:00 +0530

    4th commit

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_launch_template" "main" {
-  name_prefix   = "${env}-${component}"
+  name_prefix   = "${var.env}-${var.component}"
   image_id      = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
