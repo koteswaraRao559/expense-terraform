1de16746766936772156f35aa7eb64c0aa48cc3d
koteswaraRao559 <koteswararao1999@gmail.com>
2024-07-22 23:31:21 +0530

    4th commit

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-yum install python3.11-pip.noarch -y | tee -a /opt/userdata.log
-pip3.11 install botocore boto3 | tee -a /opt/userdata.log
+yum install python3.12-pip.noarch -y | tee -a /opt/userdata.log
+pip3.12 install botocore boto3 | tee -a /opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/koteswaraRao559/expense-ansible expense.yml -e role_name = ${role_name} -e env= ${env} | tee -a /opt/userdata.log
